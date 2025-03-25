b6ecb855d4f21b57f46644cbf230bcb30c6497f0
Yoseph Tapano <90330224+Jo2831@users.noreply.github.com>
2025-03-25 15:16:32 +0400

    Update AboutSection.jsx

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -11,9 +11,9 @@ const TAB_DATA = [
     content: (
       <ul className="list-disc pl-2">
         <li>C</li>
+        <li>C++</li>
         <li>Python</li>
         <li>Typescript</li>
-        <li>Solidity</li>
         <li>Html</li>
         <li>Css</li>
         <li>JavaScript</li>
@@ -27,10 +27,9 @@ const TAB_DATA = [
       <ul className="list-disc pl-2">
         <li>Node.js</li>
         <li>Express</li>
-        <li>React</li>
         <li>NextJs</li>
         <li>Flask</li>
-        <li>tailwind css</li>
+        <li>Django</li>
         <li>bootstrap</li>
         <li>Rest-Api</li>
       </ul>
@@ -56,11 +55,13 @@ const TAB_DATA = [
       <ul className="list-disc pl-2">
         <li>42 Abu Dhabi</li>
         <p>Associate&apos;s degree, Software development</p>
+        <p>Completed projects in Linux systems, cybersecurity, web development.</p>
+        <p>Developed custom shells, game engines, real-time applications, and microservices</p>
         <p>2023- 2025</p>
-        <li>ALX Software Engineering Program</li>
-        <p>Certificatie of Software Engineer,From Holberton Coding School.</p>
-        <p>2021-2022</p>
-        
+        <li>ALX Africa </li>
+        <p>Certificatie of Software Engineer.</p>
+        <p>Completed a one-year, intensive full-stack development program.</p>
+        <p>2022-2023</p>
       </ul>
     ),
   },
@@ -69,9 +70,16 @@ const TAB_DATA = [
     id: "certifications",
     content: (
       <ul className="list-disc pl-2">
-        <li>Certeficate of Software Engineer from ALX with Holberton School</li>
+        <li>Certeficate of Software Engineer from ALX colabration with Holberton School</li>
         <Link
               href="https://intranet.alxswe.com/certificates/R7YSZnxJXG"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
+            >
+              verify Certeficate
+        </Link>
+        <li>Certificate for ALX AI Starter Kit</li>
+        <Link
+              href="https://intranet.alxswe.com/certificates/rPN72LfYmM"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
               verify Certeficate
@@ -79,13 +87,6 @@ const TAB_DATA = [
         <li>MindLuster NextJS Course certifications</li>
         <Link
               href="https://www.mindluster.com/student/certificate/11307571887"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
-            >
-              verify Certeficate
-        </Link>
-      <li> Meta Introduction to Front-End Development Course certificate</li>
-      <Link
-              href="https://www.coursera.org/account/accomplishments/certificate/KGE66BJ992JV"
               className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-200 text-white"
             >
               verify Certeficate
@@ -112,10 +113,9 @@ const AboutSection = () => {
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-          I a{'\''}m  a software engineer skilled in both frontend and backend development.
-           I completed the ALX Software Engineering Program in collaboration with Holberton School,
-            gaining valuable hands-on experience and a knack for delivering high-quality projects on time,
-           I have a passion for continuous learning and enjoy embracing new challenges. As a quick learner, I{'\''}m always excited to take on software projects and bring positive value to each one. If you have any questions or want to explore potential collaborations, feel free to reach out. Let{'\''}s connect and create something amazing together!
+          Software Engineer specializing in backend development, AI, and full-stack web applications. 
+          Passionate about building scalable and secure systems using Django, Node.js, and Next.js. 
+            Experienced in real-time applications, microservices. Open to opportunities in software engineering, AI development, and backend infrastructure.
           </p>
           <div className="flex flex-row justify-start mt-8">
           <TabButton
