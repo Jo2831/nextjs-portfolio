58b22ac680e0b397eb86731931999bd35fb0ef7b
Yoseph Tapano <90330224+Jo2831@users.noreply.github.com>
2025-03-26 15:02:25 +0400

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -84,7 +84,7 @@ const HeroSection = () => {
         >
           <div className=" bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/josi.jpg"
+              src="/images/josi.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
